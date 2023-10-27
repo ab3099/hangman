fd97e9f24269abdde6a3bbb4ac8035cd15a6a655
Anna <annabarbano8@gmail.com>
2023-10-27 15:42:29 +0100

    check_guess and ask_for_input functions to check if the letter suggested by user is part of the word randomly chosrn by the computer

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,29 @@ print(fruit_list)
 #random choice
 word = random.choice(fruit_list)
 print(word)
+guess = input("Enter a single letter:")
+
+
+def check_guess(guess):
+    
+    guess = guess.lower()
+    if guess in word: 
+     print("Good guess! ' " + guess + " ' is in the word.")
+    else: 
+     print("Sorry, ' " + guess + " ' is not in the word. Try again.")
+
+def ask_for_input(guess):
+ while True:
+    
+    
+    if len(guess) == 1 and guess.isalpha():
+      print("You've entered a valid character") 
+      break 
+    else: 
+      print("Invalid letter. Please, enter a single alphabetical character.")
+
+check_guess(guess)
+
+
+test_1= ask_for_input(guess)
+
